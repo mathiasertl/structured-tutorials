d26c64d81897d5461c93ae2f22570adb02024c86
Mathias Ertl <mati@er.tl>
2026-04-02 21:00:53 +0200

    update support for sphinx 9.1

--- tests/runners/vagrant/conftest.py
+++ tests/runners/vagrant/conftest.py
@@ -3,6 +3,9 @@
 
 """conftest for vagrant tests."""
 
+from collections.abc import Iterator
+from unittest.mock import patch
+
 import pytest
 
 from structured_tutorials.models import TutorialModel
@@ -13,3 +16,10 @@ from structured_tutorials.runners.vagrant import VagrantRunner
 def runner(tutorial: TutorialModel) -> VagrantRunner:
     """Fixture to retrieve a local tutorial runner based on the tutorial fixture."""
     return VagrantRunner(tutorial, interactive=False)
+
+
+@pytest.fixture(autouse=True)
+def mock_shutil_which() -> Iterator[None]:
+    """Mock shutil.which to make sure that vagrant is always "found"."""
+    with patch("structured_tutorials.runners.vagrant.shutil.which", return_value=True):
+        yield
